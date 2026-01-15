c8f2470943842a339b556c93c232ac28a2c4510b
Paul Deitel <paul@deitel.com>
2026-01-14 22:31:34 -0500

    Updates for January 2026 course

--- PythonFullThrottle/ch05/fig05_01.py
+++ PythonFullThrottle/ch05/fig05_01.py
@@ -6,7 +6,7 @@ print('\nCreating a bar chart from numbers:')
 print(f'Index{"Value":>8}   Bar')
 
 for index, value in enumerate(numbers):
-    print(f'{index:>5}{value:>8}   {"*" * value}')
+    print(f'{index:>5}{value:>8}   {chr(0x2588) * value}')
 
 
 #**************************************************************************
--- PythonFullThrottle/ch06/fig06_02.py
+++ PythonFullThrottle/ch06/fig06_02.py
@@ -1,13 +1,15 @@
 # fig06_02.py
 """Tokenizing a string and counting unique words."""
 
-text = ('this is sample text with several words ' 
+text = ('this is sample text with several words '
         'this is more sample text with some different words')
 
 word_counts = {}
 
 # count occurrences of each unique word
 for word in text.split():
+    # could replace this if/else with: 
+    # word_counts[word] = word_counts.get(word, 0) + 1
     if word in word_counts: 
         word_counts[word] += 1  # update existing key-value pair
     else:
